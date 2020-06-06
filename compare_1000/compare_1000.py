a25f52b563deeb017b209cb2195fca11b66870a4
trusov <trusovii@mail.ru>
2020-06-06 12:50:32 +0300

    Added pdf output

--- compare_1000/compare_1000.py
+++ compare_1000/compare_1000.py
@@ -1,4 +1,4 @@
-import inspect
+﻿import inspect
 
 import numpy
 import os
@@ -548,14 +548,12 @@ def create_parser():
     compare_1000.py
     path - input path to benchmark-reports' folder
     cmd - output in command line
-        -f, --full - full output
-
-    not supported yet:
-        -a, --analysis - analysis output
+        [-f, --full - full output
+         -a, --analysis - analysis output]
     pdf - output in pdf-file
         output_path - path for output pdf-file
-        -f, --full - full output
-        -a, --analysis - analysis output
+        [-f, --full - full output
+         -a, --analysis - analysis output]
         -d, --delete_png - delete all graph files
     """
     parser = argparse.ArgumentParser(
